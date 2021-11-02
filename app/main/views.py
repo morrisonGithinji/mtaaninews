5e464550170db9760eac447ec6543ce32566dd85
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-11-02 11:48:56 +0300

    define news on views

---
+++ app/main/views.py
@@ -0,0 +1,19 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_sources,get_source
+
+
+@main.route('/')
+def News():
+  '''
+  returns News page and data
+  '''
+
+  #Getting gen news
+  general_news = get_sources('general')
+  #Getting sports news
+  sports_news = get_sources('sports')
+  #Getting entertainment news
+  entertainment_news = get_sources ('entertainment')
+  
+  return render_template('sources.html',general =general_news,sports= sports_news,entertainment=entertainment_news)
