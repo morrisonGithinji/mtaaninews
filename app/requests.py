fa7f0f3bce4d2da8f5df472e63b0196188fea4a8
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-11-02 09:59:50 +0300

    define get sources

--- app/requests.py
+++ app/requests.py
@@ -16,3 +16,20 @@ def configure_request(app):
   base_url =app.config['NEWS_API_BASE_URL']
   article_url =app.config['ARTICLES_API_BASE']
   all_articles =app.config['ALL_ARTICLES_API']
+
+def get_sources(category):
+  
+  get_sources_url =  base_url.format(category,api_key)  
+  
+  with urllib.request.urlopen(get_sources_url) as url:
+    get_sources_data = url.read()
+    get_sources_response = json.loads(get_sources_data)
+    
+    sources_results = None
+    
+    if get_sources_response ['sources']:
+      sources_results_list = get_sources_response['sources']
+      sources_results = process_results(sources_results_list)
+      
+      
+  return sources_results  
